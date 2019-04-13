a40798070fda863e19e8f575767328570536f505
danimrprofe <danimrprofe@gmail.com>
2019-04-13 09:26:20 +0200

    creado proyecto despliegue cluster k8s

---
+++ vagrant-k8s/aprovisionamiento-nodos.sh
@@ -0,0 +1,4 @@
+    echo "This is worker"
+    apt-get install -y sshpass
+    sshpass -p "vagrant" scp -o StrictHostKeyChecking=no vagrant@192.168.205.10:/etc/kubeadm_join_cmd.sh .
+    sh ./kubeadm_join_cmd.sh
