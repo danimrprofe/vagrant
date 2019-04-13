3c8a328d2cd062e8d44495bb1ea41dff404947db
danimrprofe <danimrprofe@gmail.com>
2019-04-13 19:26:00 +0200

    en proceso

--- vagrant-k8s/aprovisionamiento-box.sh
+++ vagrant-k8s/aprovisionamiento-box.sh
@@ -1,25 +1,29 @@
+    
+
+    
     # install docker v17.03
     # reason for not using docker provision is that it always installs latest version of the docker, but kubeadm requires 17.03 or older
-    apt-get update
-    apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-    add-apt-repository "deb https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") $(lsb_release -cs) stable"
-    apt-get update && apt-get install -y docker-ce=$(apt-cache madison docker-ce | grep 17.03 | head -1 | awk '{print $3}')
+    
+    # Agregamos el repositorio para poder descargar kubernetes
+    curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+    apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+
+    # Actualizamos lista de paquetes
+    apt update
+
+    # Instalamos los paquetes necesari
+    apt install -y apt-transport-https ca-certificates curl software-properties-common
+    apt install -y docker.io
+    apt-get install -y kubelet kubeadm kubectl
+    
+
     # run docker commands as vagrant user (sudo not required)
     usermod -aG docker vagrant
-    # install kubeadm
-    apt-get install -y apt-transport-https curl
-    curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-    cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-    deb http://apt.kubernetes.io/ kubernetes-xenial main
-EOF
-    apt-get update
-    apt-get install -y kubelet kubeadm kubectl
-    apt-mark hold kubelet kubeadm kubectl
-    # kubelet requires swap off
-    swapoff -a
-    # keep swap off after reboot
-    sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+    systemctl enable docker
+
+  
+
+    
     # ip of this box
     IP_ADDR=`ifconfig enp0s8 | grep Mask | awk '{print $2}'| cut -f2 -d:`
     # set node-ip
--- vagrant-k8s/aprovisionamiento-master.sh
+++ vagrant-k8s/aprovisionamiento-master.sh
@@ -1,6 +1,7 @@
     echo "This is master"
     # ip of this box
     IP_ADDR=`ifconfig enp0s8 | grep Mask | awk '{print $2}'| cut -f2 -d:`
+    echo "IP master: $IP_ADDR"
     # install k8s master
     HOST_NAME=$(hostname -s)
     kubeadm init --apiserver-advertise-address=$IP_ADDR --apiserver-cert-extra-sans=$IP_ADDR  --node-name $HOST_NAME --pod-network-cidr=172.16.0.0/16
@@ -9,11 +10,17 @@
     cp -i /etc/kubernetes/admin.conf /home/vagrant/.kube/config
     chown $(id -u vagrant):$(id -g vagrant) /home/vagrant/.kube/config
     # install Calico pod network addon
+    
+    kubectl apply -f https://docs.projectcalico.org/v3.0/getting-started/kuberentes/installation/hosted/kubeadm/1.7/calico.yaml
     export KUBECONFIG=/etc/kubernetes/admin.conf
-    kubectl apply -f https://raw.githubusercontent.com/ecomm-integration-ballerina/kubernetes-cluster/master/calico/rbac-kdd.yaml
-    kubectl apply -f https://raw.githubusercontent.com/ecomm-integration-ballerina/kubernetes-cluster/master/calico/calico.yaml
+    
+
     kubeadm token create --print-join-command >> /etc/kubeadm_join_cmd.sh
     chmod +x /etc/kubeadm_join_cmd.sh
     # required for setting up password less ssh between guest VMs
     sudo sed -i "/^[^#]*PasswordAuthentication[[:space:]]no/c\PasswordAuthentication yes" /etc/ssh/sshd_config
     sudo service sshd restart
+
+    # Instalar el dashboard
+    #kubectl create -f https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/recommended/kubernetes-dashboard.yaml
+    #kubectl proxy
